40d031086b74f100b7beea77303349e07ce103dd
kKapalka <k.kapalka@hotmail.com>
2024-01-13 19:14:22 +0100

    settings menu panel

--- scripts/ui/menu.gd
+++ scripts/ui/menu.gd
@@ -17,3 +17,13 @@ func _on_confirmation_popup(response):
 		main.switch_state(global.GAME_WORLD)
 	
 	$ConfirmationPopup.visible = false
+
+
+func _on_return_button_button_up():
+	$Panel/MarginContainer/VBoxContainer/SettingsMenuPanel.visible = false
+	$Panel/MarginContainer/VBoxContainer/MainMenuPanel.visible = true
+
+
+func _on_options_button_up():
+	$Panel/MarginContainer/VBoxContainer/MainMenuPanel.visible = false
+	$Panel/MarginContainer/VBoxContainer/SettingsMenuPanel.visible = true
