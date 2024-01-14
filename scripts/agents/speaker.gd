73d85f58f36eb27d914e9c8aa3b7de824447e019
kKapalka <k.kapalka@hotmail.com>
2024-01-14 18:46:21 +0100

    editable sprite2D, collider2D

--- scripts/agents/speaker.gd
+++ scripts/agents/speaker.gd
@@ -1,11 +1,24 @@
-extends CharacterBody2D
+@tool
+extends StaticBody2D
 
 class_name Speaker
 
 @export var speakerName: String = ""
 @export var dialogue: DialogueResource
+@export var defaultSprite: Texture2D = null : set = setTexture
+@export var collider: Shape2D = null : set = setCollisionShape
+
 @onready var talkPanel: Panel = $TalkPanel
 @onready var talkPanelLabel: Label = $TalkPanel/Label
+
+func setTexture(newTexture: Texture2D):
+	defaultSprite = newTexture
+	$Sprite2D.texture = defaultSprite
+
+func setCollisionShape(newCollider: Shape2D):
+	collider = newCollider
+	$CollisionShape2D.shape = newCollider
+	$Area2D/CollisionShape2D.shape = newCollider
 
 func onAreaEntered():
 	talkPanel.visible = true
@@ -14,7 +27,6 @@ func onAreaExited():
 
 func _ready():
 	talkPanelLabel.text = "["+InputMap.action_get_events("interact")[0].as_text().substr(0, 1)+"] Talk"
-
 
 func onInteract():
 	var balloon = DialogueManager.show_example_dialogue_balloon(dialogue, "start")
