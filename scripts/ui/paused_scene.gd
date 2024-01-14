2c15d5335d783c915940dc9c822ad8bcba2e1e7b
Resben <bendavidhawkins@gmail.com>
2024-01-15 00:36:15 +0800

    [Feature] Exit + Options for paused menu

--- scripts/ui/paused_scene.gd
+++ scripts/ui/paused_scene.gd
@@ -1,11 +1,25 @@
 extends Control
 
+func _ready():
+	$MarginContainer/Panel/MarginContainer/VBoxContainer/SettingsMenuPanel.returnButton.button_up.connect(_on_return_button_button_up)
+
 func _on_continue_button_up():
 	var main = get_node("/root/Main") as Manager
 	main.switch_to_last_state()
 
 func _on_options_button_up():
-	pass # Replace with function body.
+	$MarginContainer/Panel/MarginContainer/VBoxContainer/PausedLabel.visible = false
+	$MarginContainer/Panel/MarginContainer/VBoxContainer/OptionsLabel.visible = true
+	$MarginContainer/Panel/MarginContainer/VBoxContainer/MainPaused.visible = false
+	$MarginContainer/Panel/MarginContainer/VBoxContainer/SettingsMenuPanel.visible = true
 
 func _on_exit_button_up():
-	pass # Replace with function body.
+	# SAVE GAME HERE
+	var main = get_node("/root/Main") as Manager
+	main.switch_state(global.MENU)
+	
+func _on_return_button_button_up():
+	$MarginContainer/Panel/MarginContainer/VBoxContainer/PausedLabel.visible = true
+	$MarginContainer/Panel/MarginContainer/VBoxContainer/OptionsLabel.visible = false
+	$MarginContainer/Panel/MarginContainer/VBoxContainer/MainPaused.visible = true
+	$MarginContainer/Panel/MarginContainer/VBoxContainer/SettingsMenuPanel.visible = false
