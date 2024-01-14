f9b25c1396aab06119cd27c7c89d4898d3be468f
Resben <bendavidhawkins@gmail.com>
2024-01-14 23:14:49 +0800

    [Feature] Return dialogue confirmation

--- scripts/CallbackHandler.gd
+++ scripts/CallbackHandler.gd
@@ -2,8 +2,12 @@ extends Node
 
 signal load_location
 
-var location_to_load = "town"
+var location_to_load = ""
 
 func LoadLocation():
 	var game_world = get_node("/root/Main/GameWorld") as GameWorld
 	game_world.switch_level(location_to_load)
+	
+func LoadMap():
+	var game_world = get_node("/root/Main/GameWorld") as GameWorld
+	game_world.switch_level("map")
