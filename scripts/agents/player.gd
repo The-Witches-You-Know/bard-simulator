a25a4a8da91d83a8bf029bbd157d298cff724f3e
kKapalka <k.kapalka@hotmail.com>
2024-01-18 12:57:32 +0100

    levels, collisions

--- scripts/agents/player.gd
+++ scripts/agents/player.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 class_name PlayerBody
 
-const SPEED = 300.0
+const SPEED = 600.0
+var topColliderOffset: int = 30
+var collisionDirection: Vector2 = Vector2(0,0)
 
 var speaker: Speaker = null
 
@@ -12,6 +14,7 @@ func _physics_process(_delta):
 	var inputY = Input.get_axis ("character_up" , "character_down")
 	var input_direction = Vector2(inputX, inputY)
 	velocity = input_direction.normalized() * SPEED
+	
 
 	if inputY == -1:
 		#$AnimationPlayer.play("up", 1, 4)
@@ -29,13 +32,21 @@ func _physics_process(_delta):
 		#$AnimationPlayer.stop()
 		pass
 	
-
-	move_and_slide()
+	# if collider is in the direction where the player is going, dont move and slide
+	if (velocity == Vector2(0, 0) and collisionDirection != Vector2(0,0)):
+		velocity = collisionDirection * -1
+	if (velocity != collisionDirection):
+		move_and_slide()
+	if get_slide_collision_count() > 0:
+		collisionDirection = get_slide_collision(0).get_normal() * -1
+		collisionDirection = velocity
+	else:
+		collisionDirection = Vector2(0,0)
 	if Input.is_action_just_pressed("interact") and speaker != null:
 		speaker.onInteract()
 		
 func setCameraLimit(boundaryMin: Vector2, boundaryMax: Vector2):
-	$Camera2D.limit_top = boundaryMin.y
+	$Camera2D.limit_top = boundaryMin.y - topColliderOffset
 	$Camera2D.limit_left = boundaryMin.x
 	$Camera2D.limit_bottom = boundaryMax.y
 	$Camera2D.limit_right = boundaryMax.x
