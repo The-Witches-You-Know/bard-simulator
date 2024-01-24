2415a14f5d44f8c3f8112c509ce2af3b8fc78fd3
Resben <bendavidhawkins@gmail.com>
2024-01-25 02:25:24 +0800

    [Refactor] Removed ambient sound

--- scripts/settings_menu_panel.gd
+++ scripts/settings_menu_panel.gd
@@ -1,8 +1,10 @@
 extends Node
 
+# Ambient sounds removed for now
+
 @onready var masterVolumeSlider: HSlider = $MasterVolume/MasterVolumeSlider
 @onready var musicVolumeSlider: HSlider = $MusicVolume/MusicVolumeSlider
-@onready var ambientVolumeSlider: HSlider = $AmbientVolume/AmbientVolumeSlider
+#@onready var ambientVolumeSlider: HSlider = $AmbientVolume/AmbientVolumeSlider
 @onready var SFXVolumeSlider: HSlider = $SFXVolume/SFXVolumeSlider
 
 @onready var returnButton: Button = $ReturnButton
@@ -10,7 +12,7 @@ extends Node
 func _ready():
 	masterVolumeSlider.value = Save_Loader.settingsData.getMasterVolume()
 	musicVolumeSlider.value = Save_Loader.settingsData.getMusicVolume()
-	ambientVolumeSlider.value = Save_Loader.settingsData.getAmbienceVolume()
+	#ambientVolumeSlider.value = Save_Loader.settingsData.getAmbienceVolume()
 	SFXVolumeSlider.value = Save_Loader.settingsData.getSFXVolume()
 
 func _on_master_volume_slider_value_changed(value):
@@ -20,8 +22,8 @@ func _on_music_volume_slider_value_changed(value):
 func _on_sfx_volume_slider_value_changed(value):
 	Save_Loader.settingsData.setSFXVolume(value)
 	Audio_Player.triggerSpeechPlayer()
-func _on_ambient_volume_slider_value_changed(value):
-	Save_Loader.settingsData.setAmbienceVolume(value)
+#func _on_ambient_volume_slider_value_changed(value):
+	#Save_Loader.settingsData.setAmbienceVolume(value)
 
 func _on_sfx_volume_slider_drag_started():
 	Audio_Player.setSpeaker("Test")
