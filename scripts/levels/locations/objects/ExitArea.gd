9aa714e1f1971416f73e993d303772040016c895
kKapalka <k.kapalka@hotmail.com>
2024-01-25 14:13:35 +0100

    speaker colliders, exitArea responsiveness

--- scripts/levels/locations/objects/ExitArea.gd
+++ scripts/levels/locations/objects/ExitArea.gd
@@ -10,16 +10,12 @@ func _ready():
 	$AnimationPlayer.play("idle")
 
 func _process(_delta):
-	if in_range and self.visible:
+	if in_range:
 		if Input.is_action_just_released("interact"):
 			if can_leave:
-				create_tween().tween_callback(func():
-					Audio_Player.setBalloonReference(DialogueManager.show_example_dialogue_balloon(load("res://dialogue/confirm_return.dialogue"), "start"))
-					).set_delay(0.4)
+				Audio_Player.setBalloonReference(DialogueManager.show_example_dialogue_balloon(load("res://dialogue/confirm_return.dialogue"), "start"))
 			else:
-				create_tween().tween_callback(func():
-					Audio_Player.setBalloonReference(DialogueManager.show_example_dialogue_balloon(load("res://dialogue/cannot_return.dialogue"), "start"))
-					).set_delay(0.4)
+				Audio_Player.setBalloonReference(DialogueManager.show_example_dialogue_balloon(load("res://dialogue/cannot_return.dialogue"), "start"))
 
 func _on_body_entered(body):
 	if (body.name == "player"):
